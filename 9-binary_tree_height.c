44713b7627d67a617bda12f649c1a7c43a2887cd
Amir Alsayed <amyralsyd367@gmail.com>
2023-10-30 18:00:53 +0200

    adding new features to tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t left_h = 0, right_h = 0;
 
-		left_h = tree->left? binary_tree_height(tree->left) + 1: 0;
-		right_h = tree->right? binary_tree_height(tree->right) + 1: 0;
+		if (tree->left)
+			left_h = binary_tree_height(tree->left) + 1;
+
+		if (tree->right)
+			right_h = binary_tree_height(tree->right) + 1;
 
 		return ((left_h > right_h? left_h: right_h));
 	}
