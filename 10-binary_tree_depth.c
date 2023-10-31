5ac8979827e08d698e6e7a080620fddb9832e60b
Amir Alsayed <amyralsyd367@gmail.com>
2023-10-31 13:25:26 +0200

    betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -12,6 +12,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
 
+	if (tree == NULL)
+		return (0);
+
 	while (tree->parent)
 	{
 		tree = tree->parent;
--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,6 +11,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t total_left = 0, total_right = 0;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree != NULL)
 	{
 		if (tree->left != NULL)
