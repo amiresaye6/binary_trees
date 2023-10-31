d1878e4d99f66a1e9c164ad2312f0d675e9c0a7e
Amir Alsayed <amyralsyd367@gmail.com>
2023-10-31 17:38:34 +0200

    betty  :(

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL ||node->parent->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
 	if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
